fb8b56b4535b76933f54aa7aa926347dca59b2b7
KiK0S <kostya.amelichev@gmail.com>
2018-08-19 14:59:49 +0200

    refactoring of locker code

--- locker/locker.ino
+++ locker/locker.ino
@@ -1,437 +1,360 @@
-#include <EEPROM.h>
 #include <FlexiTimer2.h>
 
-const int V_MEN_R = 55;
-const int V_MEN_G = 54;
-const int V_DOOR_R = 56;
-const int V_DOOR_G = 57;
-const int V_LOCKER_R = 58;
-const int V_LOCKER_G = 59;
+namespace constant_pins {
+	const int V_MEN_R = 55;
+	const int V_MEN_G = 54;
+	const int V_DOOR_R = 56;
+	const int V_DOOR_G = 57;
+	const int V_LOCKER_R = 58;
+	const int V_LOCKER_G = 59;
 
-const int OUTSIDE_BUTTON = 31;
-const int OUTSIDE_R = 44;
-const int OUTSIDE_G = 45;
-const int OUTSIDE_B = 46;
+	const int OUTSIDE_BUTTON = 31;
+	const int OUTSIDE_RED = 44;
+	const int OUTSIDE_GREEN = 45;
+	const int OUTSIDE_BLUE = 46;
 
-const int LOCKER = 12;
+	const int LOCKER = 12;
 
-const int SOUND_OUTSIDE = 10;
-const int SOUND_INSIDE = 11;
+	const int SOUND_OUTSIDE = 10;
+	const int SOUND_INSIDE = 11;
 
-const int DOOR_SENSOR = 6;
-const int LOCKER_SENSOR = 7;
-const int EXIT_BUTTON = 8;
+	const int DOOR_SENSOR = 6;
+	const int LOCKER_SENSOR = 7;
+	const int EXIT_BUTTON = 8;
 
-const int INSIDE_SENSOR_0_0 = 2;
-const int INSIDE_SENSOR_0_1 = 3;
-const int INSIDE_SENSOR_1_0 = 4;
-const int INSIDE_SENSOR_1_1 = 5;
-
-int Num;
-int Num1;
-int w[8];
-int w1[8];
-int R;
-int G;
-int B;
-int n;
-
-long long start_time;
-long long last_got;
-long long timer;
-long long anothertimer;
-long long state_buzzer;
-
-int master[8] = {
-    55, 57, 55, 57, 48, 55, 55, 57
-}; //0 -> 48
-
-int badsymb[9] = {
-    -1, 2, 3, 10, 13, 249, 191, 235, 0
-};
-int qqLength = 4;
-int qq[4][8] = {
-    {66, 66, 57, 53, 65, 53, 66, 54},  //Vahta
-    {69, 54, 57, 55, 49, 69, 70, 57},  //Paramonov
-    {68, 57, 53, 52, 48, 57, 52, 57},  //Amelichev
-    {68, 57, 52, 49, 69, 51, 52, 57}   //Filippov
-};
-
-int q[60][8];
-
-bool rec;
-
-const int COUNT_NOTES = 9;
-
-int frequences[COUNT_NOTES] = {
-    392, 392, 392, 311, 466, 392, 311, 466, 392
-};
-
-int durations[COUNT_NOTES] = {
-    350, 350, 350, 250, 100, 350, 250, 100, 700
-};
-
-
-void led(int a, int b, int c){
-    analogWrite(OUTSIDE_R, a);
-    analogWrite(OUTSIDE_G, b);
-    analogWrite(OUTSIDE_B, c);
+	const int INSIDE_SENSOR_0_0 = 2;
+	const int INSIDE_SENSOR_0_1 = 3;
+	const int INSIDE_SENSOR_1_0 = 4;
+	const int INSIDE_SENSOR_1_1 = 5;
 }
 
-bool isAnybodyInside(){
-    bool f = 0;
-    for(int i = INSIDE_SENSOR_0_0; i <= INSIDE_SENSOR_1_1; i++){
-        f |= digitalRead(i);
-    }
-    if(timer > millis()){
-        timer = millis();
-    }
-    if(f){
-        timer = millis();
-    }
-    if(millis() - timer > 60000){
-        return 0;
-    }
-    return 1;
+namespace door_bell {
+	long long timer;
+	const int COUNT_NOTES = 9;
+	const int WAITING_TIME = 5000;
+
+	const int FREQUENCES[COUNT_NOTES] = {
+	    392, 392, 392, 311, 466, 392, 311, 466, 392
+	};
+	const int DURATIONS[COUNT_NOTES] = {
+	    350, 350, 350, 250, 100, 350, 250, 100, 700
+	};
+
+	void play() {
+	    if (millis() < timer) {
+	        timer = millis();
+	        return;
+	    }
+	    if (digitalRead(constant_pins::OUTSIDE_BUTTON) || millis() - timer < WAITING_TIME) {
+	    	return;
+	    }
+	    for (int i = 0; i < COUNT_NOTES; i++) {
+	        tone(constant_pins::SOUND_INSIDE, FREQUENCES[i], DURATIONS[i]);
+	        delay(DURATIONS[i]);
+	    }
+	    noTone(constant_pins::SOUND_INSIDE);
+	    timer = millis();
+	}
 }
 
-void Vahta(){
-    if(!isAnybodyInside()){
-        digitalWrite(V_MEN_G, 1);
-        digitalWrite(V_MEN_R, 0);
-    }
-    else{
-        digitalWrite(V_MEN_G, 0);
-        digitalWrite(V_MEN_R, 1);
-    }
-    if(digitalRead(DOOR_SENSOR)){
-        digitalWrite(V_DOOR_G, 1);
-        digitalWrite(V_DOOR_R, 0);
-    }
-    else{
-        digitalWrite(V_DOOR_G, 0);
-        digitalWrite(V_DOOR_R, 1);
-    }
-    if(digitalRead(LOCKER_SENSOR)){
-        digitalWrite(V_LOCKER_G, 1);
-        digitalWrite(V_LOCKER_R, 0);
-    }
-    else{
-        digitalWrite(V_LOCKER_G, 0);
-        digitalWrite(V_LOCKER_R, 1);
-    }
+namespace outside_led {
+	const int RED[] = {255, 0, 0};
+	const int GREEN[] = {0, 255, 0};
+	const int BLUE[] = {0, 0, 255};
+	int current_red;
+	int current_green;
+	int current_blue;
+
+	void led(int red, int green, int blue) {
+	    analogWrite(constant_pins::OUTSIDE_RED, red);
+	    analogWrite(constant_pins::OUTSIDE_GREEN, green);
+	    analogWrite(constant_pins::OUTSIDE_BLUE, blue);
+	}
+
+	void red() {
+		led(RED[0], RED[1], RED[2]);
+	}
+
+	void green() {
+		led(GREEN[0], GREEN[1], GREEN[2]);
+	}
+
+	void blue() {
+		led(BLUE[0], BLUE[1], BLUE[2]);
+	}
+
+	void recolor() {
+		if (current_red > 0 && current_green == 0) {
+		    current_red--;
+		    current_green++;
+		} 
+		else if (current_green > 0) {
+		    current_green--;
+		    current_blue++;
+		} 
+		else if (current_blue > 0) {
+		    current_blue--;
+		    current_red++;
+		}
+	}
 }
 
-void setLocked(bool b) {
-    digitalWrite(LOCKER, b);
+namespace locker {
+	const int LOCK = 1;
+	const int UNLOCK = 0;
+
+	void lock() {
+		digitalWrite(constant_pins::LOCKER, LOCK);
+	}
+
+	void unlock() {
+		digitalWrite(constant_pins::LOCKER, UNLOCK);
+	}
+
+	int locker_status() {
+		return digitalRead(constant_pins::LOCKER_SENSOR);
+	}
+
+	int door_status() {
+		return digitalRead(constant_pins::DOOR_SENSOR);
+	}
 }
 
-void play(){
-    if(millis() < anothertimer){
-        anothertimer = millis();
-        return;
-    }
-    if(digitalRead(OUTSIDE_BUTTON) || millis() - anothertimer < 5000) return;
-    for(int i = 0; i < COUNT_NOTES; i++){
-        tone(SOUND_INSIDE, frequences[i], durations[i]);
-        delay(durations[i]);
-    }
-    noTone(SOUND_INSIDE);
-    anothertimer = millis();
-}
-
-void test_button(){
-    if(!digitalRead(OUTSIDE_BUTTON)){
-        digitalWrite(SOUND_OUTSIDE, state_buzzer);
-        state_buzzer ^= 1;
-    }
-    else{
-        digitalWrite(SOUND_OUTSIDE, 0);
-    }
-}
-
-int to8(int i, int j){
-    return 8 * i + j;
-}
-
-void updN(){
-    n = EEPROM[1023];
-    if(n == 255){
-        EEPROM[1023] = 0;
-        updN();
-    }
-    if(n > 60){
-        //alert
-    }
-    n = min(n, 60);
-}
-
-bool checkCard(int a[], int b[]){
-    for(int i = 0; i < 8; i++){
-        if(a[i] != b[i]){
-            return false;
-        }
-    }
-    return true;
-}
-
-void rewriteQ(){
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < 8; j++){
-            q[i][j] = EEPROM[to8(i, j)];
-        }
-    }
-}
-
-void writeQ(){
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < 8; j++){
-            Serial.write(q[i][j]);
-            Serial.print(" ");
-        }
-        Serial.println();
-    }
-}
-
-bool isvalid(int a){
-    for(int i = 0; i < 9; i++){
-        if(a == badsymb[i]) return 0;
-    }
-    return 1;
-}
-
-void checkBtn(){
-    int p = digitalRead(EXIT_BUTTON);
-    if(p != 0){
-        setLocked(0);
-        led(0, 255, 0);
-        delay(5000);
-        setLocked(0);
-        led(0, 0, 0);
-    }
-}
-
-void writeNew(int card[]){
-    for(int i = 0; i < 8; i++){
-        EEPROM[to8(n, i)] = card[i];
-        q[n][i] = card[i];
-    }
-    EEPROM[1023]++;
-    updN();
-}
-
-void endOfReading(){
-    led(255, 255, 255);
-    delay(5000);
+namespace exit_button {
+	void check() {
+	    int status = digitalRead(constant_pins::EXIT_BUTTON);
+	    if (status != 0) {
+	       	locker::unlock();
+	        outside_led::green();
+	        delay(5000);
+	        locker::unlock();
+	    }
+	}
 }
 
 
-void del(int idx){
-    for(int i = idx; i < n - 1; i++){
-        for(int j = 0; j < 8; j++){
-            EEPROM[to8(i, j)] = EEPROM[to8(i + 1, j)];
-        }
-    }
-    EEPROM[1023]--;
-    rewriteQ();
-    updN();
+namespace security {
+	long long timer;
+
+	int cabinet_status() {
+		bool status = 0;
+		for (int i = constant_pins::INSIDE_SENSOR_0_0; i <= constant_pins::INSIDE_SENSOR_1_1; i++) {
+		    status |= digitalRead(i);
+		}
+		if (timer > millis()) {
+		    timer = millis();
+		}
+		if (status) {
+		    timer = millis();
+		}
+		if (millis() - timer > 60000) {
+		    return 1;
+		}
+		return 0;
+	}
+
+	void update() {
+	    if (cabinet_status()) {
+	        digitalWrite(constant_pins::V_MEN_G, 1);
+	        digitalWrite(constant_pins::V_MEN_R, 0);
+	    }
+	    else {
+	        digitalWrite(constant_pins::V_MEN_G, 0);
+	        digitalWrite(constant_pins::V_MEN_R, 1);
+	    }
+	    if (locker::door_status()) {
+	        digitalWrite(constant_pins::V_DOOR_G, 1);
+	        digitalWrite(constant_pins::V_DOOR_R, 0);
+	    }
+	    else {
+	        digitalWrite(constant_pins::V_DOOR_G, 0);
+	        digitalWrite(constant_pins::V_DOOR_R, 1);
+	    }
+	    if (locker::locker_status()) {
+	        digitalWrite(constant_pins::V_LOCKER_G, 1);
+	        digitalWrite(constant_pins::V_LOCKER_R, 0);
+	    }
+	    else {
+	        digitalWrite(constant_pins::V_LOCKER_G, 0);
+	        digitalWrite(constant_pins::V_LOCKER_R, 1);
+	    }
+	}
 }
 
-int _find(int a[]){
-    for(int i = 0; i < n; i++){
-        if(checkCard(q[i], a)){
-            return i;
-        }
-    }
-    return -1;
+namespace client {
+	String make_request(String type, int card[]) {
+		String res = type;
+		for (int i = 0; i < 8; i++) {
+	        res += (char)card[i];
+	    }
+	    return res;
+	}
+
+	void greeting(int card[]) {
+	    Serial3.print(make_request("}", card));
+	}
+
+	void edit(int card[]) {
+	    Serial3.print(make_request("~", card));
+	}
+
+	void check(int card[]) {
+		Serial3.print(make_request("|", card));
+	}
+
+	void receive() {
+	    if (Serial3.available()) {
+	        int ans = Serial3.read();
+	        if (ans == 89) {
+	            locker::unlock();
+	            outside_led::green();
+	            delay(5000);
+	            locker::lock();
+	        }
+	        if (ans == 78) {
+	            outside_led::red();
+	            delay(2000);
+	        }
+	    }
+	}
 }
 
-void initiation(){
-    EEPROM[1023] = 0;
-    updN();
-    for(int i = 0; i < 3; i++){
-        writeNew(qq[i]);
-    }
+namespace handler {
+	int position;
+	int buffer[8];
+	bool edit_query;
+
+	const int MASTER_CARD[8] = {
+	    55, 57, 55, 57, 48, 55, 55, 57
+	}; //0 -> 48
+	const int BAD_SYMBOLS_CNT = 9;
+	const int BAD_SYMBOLS[9] = {
+	    -1, 2, 3, 10, 13, 249, 191, 235, 0
+	};
+	const int LOCAL_DB_SIZE = 4;
+	const int DEFINED_CARDS[4][8] = {
+	    {66, 66, 57, 53, 65, 53, 66, 54},  //Vahta
+	    {69, 54, 57, 55, 49, 69, 70, 57},  //Paramonov
+	    {68, 57, 53, 52, 48, 57, 52, 57},  //Amelichev
+	    {68, 57, 52, 49, 69, 51, 52, 57}   //Filippov
+	};
+
+	bool check_card(int a[], int b[]) {
+	    for (int i = 0; i < 8; i++) {
+	        if (a[i] != b[i]) {
+	            return false;
+	        }
+	    }
+	    return true;
+	}
+
+	bool is_valid(int a) {
+	    for (int i = 0; i < BAD_SYMBOLS_CNT; i++) {
+	        if (a == BAD_SYMBOLS[i]) {
+	        	return false;
+	    	}
+	    }
+	    return true;
+	}
+
+	void handle_card(int card[]) {
+	    bool ok = false;
+	    for (int i = 0; i < LOCAL_DB_SIZE; i++) {
+	        ok = check_card(DEFINED_CARDS[i], card);
+	        if (ok) {
+	        	client::greeting(card);
+	            locker::unlock();
+	            outside_led::green();
+	            delay(5000);
+	            locker::lock();
+	            return;
+	        }
+	    }
+	    client::check(card);
+	}
+
+	void handle(int card[]) {
+		if (check_card(card, MASTER_CARD)) {
+			edit_query = true;
+			outside_led::blue();
+			delay(5000);	
+		}
+		else {
+			if (edit_query) {
+				//TODO: WebServer request
+				client::edit(card);
+				edit_query = false;
+			}
+			else {
+				handle_card(card);
+			}
+		}
+	}
+
+	void read() {
+		int value = Serial.read();
+	    if (!Serial.available()) {
+	        position = 0;
+	        return;
+	    }
+		if (is_valid(value)) {
+			buffer[position++] = value;
+			if (position == 8) {
+				position = 0;
+			}
+		}
+	}
 }
 
-void reColor(){
-    if(R > 0 && B == 0){
-        R--;
-        G++;
-    } else if(G > 0){
-        G--;
-        B++;
-    } else if(B > 0){
-        B--;
-        R++;
-    }
+void interrupt() {
+    security::update();
+    outside_led::recolor();
 }
 
-void webServerAddReq(){
-    String req = "~";
-    for (size_t i = 0; i < 8; i++) {
-        char temp = w[i];
-        req += temp;
-    }
-    Serial3.print(req);
-}
-
-void checkForOpen(){
-    if (Serial3.available()) {
-        int ans = Serial3.read();
-        if(ans == 89){
-            setLocked(0);
-            led(0, 255, 0);
-            delay(5000);
-            setLocked(1);
-            led(0, 0, 0);
-            return;
-        }
-        if(ans == 78){
-            led(255, 0, 0);
-            delay(2000);
-            led(0, 0, 0);
-        }
-    }
-}
-
-void checkCards(){
-    bool ok = false;
-    for(int i = 0; i < qqLength; i++){
-        ok = checkCard(qq[i], w);
-        if(ok){
-            SayHello();
-            setLocked(0);
-            led(0, 255, 0);
-            delay(5000);
-            setLocked(1);
-            led(0, 0, 0);
-            return;
-        }
-    }
-    checkWebCards();
-}
-
-void SayHello() {
-    String req = "}";
-    for (size_t i = 0; i < 8; i++) {
-        char temp = w[i];
-        req += temp;
-    }
-    Serial3.print(req);
-}
-
-void checkWebCards(){
-    String req = "|";
-    for (size_t i = 0; i < 8; i++) {
-        char temp = w[i];
-        req += temp;
-    }
-    Serial3.print(req);
-}
-
-void(* reboot)(void) = 0;
-
-void interrupt(){
-    Vahta();
-    reColor();
-    test_button();
-}
-
-void setup(){
+void setup() {
     Serial.begin(9600);
     Serial3.begin(115200);
-    //Serial3.begin(9600);
-    if(EEPROM[1023] == 255){
-        initiation();
-    }
-    updN();
-    rewriteQ();
-    //writeQ();
-    pinMode(DOOR_SENSOR, INPUT); // защелка
-    pinMode(LOCKER_SENSOR, INPUT); // дверь
-    digitalWrite(DOOR_SENSOR,1);
-    digitalWrite(LOCKER_SENSOR,1);
-    pinMode(EXIT_BUTTON, INPUT);
-    pinMode(LOCKER, OUTPUT);
-    pinMode(OUTSIDE_R, OUTPUT);
-    pinMode(OUTSIDE_G, OUTPUT);
-    pinMode(OUTSIDE_B, OUTPUT);
+    pinMode(constant_pins::DOOR_SENSOR, INPUT); // защелка
+    pinMode(constant_pins::LOCKER_SENSOR, INPUT); // дверь
+    digitalWrite(constant_pins::DOOR_SENSOR, 1);
+    digitalWrite(constant_pins::LOCKER_SENSOR, 1);
+    pinMode(constant_pins::EXIT_BUTTON, INPUT);
+    pinMode(constant_pins::LOCKER, OUTPUT);
+    pinMode(constant_pins::OUTSIDE_RED, OUTPUT);
+    pinMode(constant_pins::OUTSIDE_GREEN, OUTPUT);
+    pinMode(constant_pins::OUTSIDE_BLUE, OUTPUT);
     pinMode(13, OUTPUT);
-    pinMode(V_MEN_G,OUTPUT);
-    pinMode(V_MEN_R,OUTPUT);
-    pinMode(V_DOOR_G,OUTPUT);
-    pinMode(V_DOOR_R,OUTPUT);
-    pinMode(V_LOCKER_G,OUTPUT);
-    pinMode(V_LOCKER_R,OUTPUT);
-    pinMode(OUTSIDE_BUTTON, INPUT);
-    digitalWrite(OUTSIDE_BUTTON,1);
-    pinMode(SOUND_OUTSIDE,OUTPUT);
-    pinMode(SOUND_INSIDE,OUTPUT);
-    pinMode(INSIDE_SENSOR_0_0, INPUT);
-    pinMode(INSIDE_SENSOR_0_1, INPUT);
-    pinMode(INSIDE_SENSOR_1_0, INPUT);
-    pinMode(INSIDE_SENSOR_1_1, INPUT);
-    timer = millis();
-    anothertimer = millis();
-    last_got = millis();
-    start_time = millis();
-    state_buzzer = 0;
-    R = 255;
-    G = 0;
-    B = 0;
-    Num = 0;
-    rec = 0;
+    pinMode(constant_pins::V_MEN_G,OUTPUT);
+    pinMode(constant_pins::V_MEN_R,OUTPUT);
+    pinMode(constant_pins::V_DOOR_G,OUTPUT);
+    pinMode(constant_pins::V_DOOR_R,OUTPUT);
+    pinMode(constant_pins::V_LOCKER_G,OUTPUT);
+    pinMode(constant_pins::V_LOCKER_R,OUTPUT);
+    pinMode(constant_pins::OUTSIDE_BUTTON, INPUT);
+    digitalWrite(constant_pins::OUTSIDE_BUTTON, 1);
+    pinMode(constant_pins::SOUND_OUTSIDE,OUTPUT);
+    pinMode(constant_pins::SOUND_INSIDE,OUTPUT);
+    pinMode(constant_pins::INSIDE_SENSOR_0_0, INPUT);
+    pinMode(constant_pins::INSIDE_SENSOR_0_1, INPUT);
+    pinMode(constant_pins::INSIDE_SENSOR_1_0, INPUT);
+    pinMode(constant_pins::INSIDE_SENSOR_1_1, INPUT);
+    security::timer = millis();
+    door_bell::timer = millis();
+    outside_led::current_red = 255;
+    outside_led::current_green = 0;
+    outside_led::current_blue = 0;
+    handler::position = 0;
+    handler::edit_query = 0;
     FlexiTimer2::set(2, interrupt);
     FlexiTimer2::start();
+    locker::lock();
 }
 
-void loop(){
-    if(millis() < start_time){
-        start_time = millis();
-    }
-    checkBtn();
-    led(R, G, B);
-    setLocked(1);
-    checkForOpen();
-    play();
-    /*if(millis() - start_time > 86400){
-        led(255, 255, 255);
-        asm volatile("jmp 0x00");
-        //reboot();
-    }*/
+void loop() {
+    exit_button::check();
+    outside_led::led(outside_led::current_red, outside_led::current_green, outside_led::current_blue);
+    client::receive();
+    door_bell::play();
+    handler::read();
     delay(2);
-
-    int a = Serial.read();
-    if(!Serial.available()){
-        Num = 0;
-        return;
-    }
-    else {
-        if(isvalid(a)){
-            last_got = millis();
-            w[Num] = a;
-            Num++;
-            if(Num == 8){
-                if(checkCard(w, master)){
-                    rec = true;
-                    led(0,0,255);
-                    delay(5000);
-                    led(0,0,0);
-                    Num = 0;
-                }
-                else{
-                    if(rec){
-                        //TODO: WebServer request
-                        webServerAddReq();
-                        rec = false;
-                    }
-                    else{
-                        checkCards();
-                    }
-                    Num = 0;
-                }
-            }
-        }
-    }
 }
