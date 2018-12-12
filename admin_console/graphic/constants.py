750f410b56e716600f0303cb5534ade7309b5f92
Unknown <gamiysikdima@gmail.com>
2018-12-12 17:19:39 +0300

    init

--- admin_console/graphic/constants.py
+++
@@ -1,6 +0,0 @@
-from tkinter import *
-root = Tk()
-card_wait_frame = Frame(root)
-options_frame = Frame(root)
-
-#btn = Button(root, {'text': 'добавить пользователя'})
--- admin_console/interfaces/graph.py
+++
@@ -1,3 +0,0 @@
-from tkinter import *
-from graphic import card_wait
-card_wait.start()
